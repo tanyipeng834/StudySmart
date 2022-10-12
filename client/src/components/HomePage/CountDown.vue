30883daf4a0ad4bf88c0396fc621c2ab92945df8
Qds11 <limqiqing96@gmail.com>
2022-10-12 16:55:01 +0800

    added count down table

--- client/src/components/HomePage/CountDown.vue
+++ client/src/components/HomePage/CountDown.vue
@@ -1,8 +1,53 @@
 <template>
+     <div>
     
+          <ul class="list-group">
+               <li  class="list-group-item d-flex justify-content-between align-items-start header bg-dark">
+                    <h5 class="text-white">Test Name</h5>
+                    <h5 class="float-end text-white">Days Left</h5>
+               </li>
+               <li v-for="test in tests" class="list-group-item d-flex justify-content-between align-items-start">
+                    <div class="ms-2 me-auto">
+                         <div class="fw-bold">{{test.testName}}</div>
+                         {{test.subject}}
+                    </div>
+                    <span class="p-2 badge bg-primary rounded-pill">{{daysLeft(test.date)}}</span>
+               </li>
+          </ul>
+     </div>
 </template>
 <script>
-export default {
-     name: "CountDown"
-}
+     export default {
+          name: "CountDown",
+          props: {
+               tests: Array,
+          },
+          methods: {
+               daysLeft(date) {
+                    let testDate = new Date(date)
+                    let currentDate=new Date()
+                    let difference = testDate.getTime() - currentDate.getTime();
+                    let TotalDays = Math.ceil(difference / (1000 * 3600 * 24));
+                    return TotalDays;
+                    
+               }
+          }
+     };
 </script>
+<style scoped>
+     @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap');
+     @import url('https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css');
+
+     * {
+
+          margin: 0;
+          padding: 0;
+          box-sizing: border-box;
+          font-family: 'Poppins', sans-serif;
+     }
+
+     ul {
+          margin-top: 20px
+     }
+   
+</style>
--- client/src/views/Dashboard.vue
+++ client/src/views/Dashboard.vue
@@ -1,35 +1,65 @@
 <template>
-    <Sidebar profileName ="Tan Yi Peng" profileRole="Secondary 3 Student"/>
+    <div class="container">
+        <Sidebar profileName="Tan Yi Peng" profileRole="Secondary 3 Student" />
 
-<CountDown />
+
+        <div class="row">
+            <div class="col-9">
+
+            </div>
+            <div class="col-3">
+                <CountDown :tests="tests" />
+            </div>
+        </div>
+    </div>
 
 </template>
 
 
 
 <script>
-import Sidebar from "../components/HomePage/Sidebar.vue"
-import CountDown from "../components/HomePage/CountDown.vue"
+    import Sidebar from "../components/HomePage/Sidebar.vue"
+    import CountDown from "../components/HomePage/CountDown.vue"
 
 
-export default{
-    name:"Dashboard",
-    components:
-    {
-        Sidebar,
-        CountDown
-    },
-        data(){
-            return {}
-        }
+    export default {
+        name: "Dashboard",
+        components: {
+            Sidebar,
+            CountDown
+        },
+        data() {
+            return {
+                tests: []
+            }
+        },
+    created() {
+            //this is preferably stored in backend and is sorted before stored
+            this.tests = [{
+                id: 1,
+                testName: 'CA1 Paper',
+                date: '11/25/2022',
+                subject: 'Maths'
+            },
+            {
+                id: 2,
+                testName: 'CA2 Paper',
+                date: '01/25/2023',
+                subject: 'Maths'
+                },
+            {
+                id: 3,
+                testName: 'SA1 Paper',
+                date: '02/25/2023',
+                subject: 'English'
+            }]
+    }
+        
 
-    
-
-
-}
 
 
 
+    }
 </script>
 
 <style scoped>
