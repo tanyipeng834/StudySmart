36891f1f94628dde36c1f1940b926b90521999b4
Yi Peng <tanyipeng835@hotmail.com>
2022-10-13 16:29:57 +0800

    working on quiz

--- client/src/views/Quiz.vue
+++ client/src/views/Quiz.vue
@@ -2,20 +2,44 @@
   <div class="container-fluid quiz">
     <Sidebar></Sidebar>
 
-    <div class="row">
-      <div class="col-3"></div>
-      <div class="col-3 mt-5"><h3 id="flash-cards">Flashcards</h3></div>
+    <div v-if="modal == false">
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-3 mt-5"><h3 id="flash-cards">Flashcards</h3></div>
+      </div>
+      <div class="row cards"></div>
+      <div class="button">
+        <button class="btn add" @click="addCard()">
+          <i class="fa-regular fa-pen-to-square fa-lg text-right"></i>Add Flash
+          Card
+        </button>
+      </div>
+    </div>
+    <div v-else>
+      <div class="row">
+        <div class="col-3"></div>
+        <div class="col-6"><h1>Create a new Study Set</h1></div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import sidebar from "../components/HomePage/Sidebar.vue";
 import Sidebar from "../components/HomePage/Sidebar.vue";
 
 export default {
   name: "Quiz",
+  data() {
+    return {
+      modal: false,
+    };
+  },
   components: { Sidebar },
+  methods: {
+    addCard() {
+      this.modal = true;
+    },
+  },
 };
 </script>
 
@@ -32,5 +56,8 @@ export default {
 #flash-cards {
   font-family: Arial, Helvetica, sans-serif;
   font-weight: 600;
+}
+.add {
+  float: right;
 }
 </style>
