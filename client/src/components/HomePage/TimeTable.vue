d359fb9604bd0203ef3fb119d9f12cef5a82e1e9
xhihyu <xhihyuxxx@gmail.com>
2022-10-15 02:49:31 +0800

    timetable15/10

--- client/src/components/HomePage/TimeTable.vue
+++ client/src/components/HomePage/TimeTable.vue
@@ -1,160 +1,146 @@
 <template>
-  <div>
-    <div class="container timetable">
-      <button class="btn">
-        <i class="fa-regular fa-pen-to-square fa-lg text-right"></i>Add Subject
-      </button>
-      <div class="timetable-img text-center">
-        <img src="img/content/timetable.png" alt="" />
-      </div>
-      <div class="table-responsive">
-        <table class="table table-bordered text-center">
-          <thead>
-            <tr class="bg-light-gray">
-              <th class="text-uppercase">Time</th>
-              <th class="text-uppercase">Monday</th>
-              <th class="text-uppercase">Tuesday</th>
-              <th class="text-uppercase">Wednesday</th>
-              <th class="text-uppercase">Thursday</th>
-              <th class="text-uppercase">Friday</th>
-              <th class="text-uppercase">Saturday</th>
-            </tr>
-          </thead>
-          <tbody></tbody>
-        </table>
-      </div>
+  <div class="wrap">
+
+    <div class="content">
+      <h1>Drag and Hold to add new schedule!</h1>
+      <DayPilotCalendar id="dp" :config="calendarConfig" ref="calendar" />
     </div>
   </div>
 </template>
+
 <script>
-import gsap from "gsap";
+import {DayPilot, DayPilotCalendar} from '@daypilot/daypilot-lite-vue'
+
+var colours = [
+  {name: "Red", id: "red"},
+  {name: "Orange", id: "orange"},
+  {name: "Yellow", id: "yellow"},
+  {name: "Blue", id: "blue"},
+  {name: "Green", id: "green"},
+  {name: "Purple", id: "purple"},
+  {name: "Pink", id: "pink"},
+  {name: "Grey", id: "grey"},
+];
+
+
+
 export default {
-  name: "TimeTable",
-  mounted() {
-    gsap.from(".timetable", { opacity: 0, Y: -50, delay: 1.0 });
-  },
+  name: 'ResourceCalendar',
+  data: function() {
+    return {
 
+      calendarConfig: {
+        viewType: "Resources",
+        timeRangeSelectedHandling: "Enabled",
+        onTimeRangeSelected: async (args) => {
+          await this.createEvent(args.start, args.end, args.resource);
+          this.calendar.clearSelection();
+        },
+        onEventClick: (args) => {
+          this.editEvent(args.e);
+        },
+        eventDeleteHandling: "Disabled",
+        onEventMoved: (args) => {
+          console.log("Event moved", args.e);
+        },
+        onEventResized: (args) => {
+          console.log("Event resized", args.e);
+        },
+      },
+    }
+  },
   props: {
-    timetable: Array,
   },
-};
+  components: {
+    DayPilotCalendar,
+
+  },
+  computed: {
+    // DayPilot.Calendar object - https://api.daypilot.org/daypilot-calendar-class/
+    calendar() {
+      return this.$refs.calendar.control;
+    },
+
+  },
+  methods: {
+    loadEvents() {
+      // placeholder for an HTTP call
+      
+      this.calendar.update({events});
+    },
+    loadResources() {
+      const columns = [
+        {name: "Monday", id: "R1"},
+        {name: "Tuesday", id: "R2"},
+        {name: "Wednesday", id: "R3"},
+        {name: "Thursday", id: "R4"},
+        {name: "Friday", id: "R5"},
+      ];
+      this.calendar.update({columns});
+    },
+    async editEvent(e) {
+      const form = [
+        {name: "Subject", id: "text"},
+        {name: "Day", id: "resource", type: "select", options: this.calendar.columns.list}
+      ];
+      const data = e.data;
+      const modal = await DayPilot.Modal.form(form, data);
+      if (modal.canceled) {
+        return;
+      }
+      else{
+      this.calendar.events.update(modal.result);
+      }
+
+    },
+    async createEvent(start, end, resource) {
+      const form = [
+        {name: "Subject", id: "text"},
+        {name: "Day", id: "resource", type: "select", options: this.calendar.columns.list}
+      ];
+      const data = {
+        start,
+        end,
+        resource,
+        id: DayPilot.guid()
+      };
+      const modal = await DayPilot.Modal.form(form, data);
+      if (modal.canceled) {
+        return;
+      }
+
+      const e = modal.result;
+      this.calendar.events.add(e);
+    }
+  },
+  mounted() {
+    this.loadResources();
+    this.loadEvents();
+  }
+}
 </script>
-<style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@200;300;400;500;600;700&display=swap");
-@import url("https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css");
 
-body {
-  margin-top: 20px;
-}
-.bg-light-gray {
-  background-color: #f7f7f7;
-}
-.table-bordered thead td,
-.table-bordered thead th {
-  border-bottom-width: 2px;
-}
-.table thead th {
-  vertical-align: bottom;
-  border-bottom: 2px solid #dee2e6;
-}
-.table-bordered td,
-.table-bordered th {
-  border: 1px solid #dee2e6;
+<style>
+.wrap {
+  display: flex;
 }
 
-.bg-sky.box-shadow {
-  box-shadow: 0px 5px 0px 0px #00a2a7;
+.left {
+  margin-right: 10px;
 }
 
-.bg-orange.box-shadow {
-  box-shadow: 0px 5px 0px 0px #af4305;
+.content {
+  flex-grow: 1;
 }
 
-.bg-green.box-shadow {
-  box-shadow: 0px 5px 0px 0px #4ca520;
+.navigator_default_busy.navigator_default_cell {
+  border-bottom: 4px solid #ee4f2ecc;
+  box-sizing: border-box;
 }
-
-.bg-yellow.box-shadow {
-  box-shadow: 0px 5px 0px 0px #dcbf02;
-}
-
-.bg-pink.box-shadow {
-  box-shadow: 0px 5px 0px 0px #e82d8b;
-}
-
-.bg-purple.box-shadow {
-  box-shadow: 0px 5px 0px 0px #8343e8;
-}
-
-.bg-lightred.box-shadow {
-  box-shadow: 0px 5px 0px 0px #d84213;
-}
-
-.bg-sky {
-  background-color: #02c2c7;
-}
-
-.bg-orange {
-  background-color: #e95601;
-}
-
-.bg-green {
-  background-color: #5bbd2a;
-}
-
-.bg-yellow {
-  background-color: #f0d001;
-}
-
-.bg-pink {
-  background-color: #ff48a4;
-}
-
-.bg-purple {
-  background-color: #9d60ff;
-}
-
-.bg-lightred {
-  background-color: #ff5722;
-}
-
-.padding-15px-lr {
-  padding-left: 15px;
-  padding-right: 15px;
-}
-.padding-5px-tb {
-  padding-top: 5px;
-  padding-bottom: 5px;
-}
-.margin-10px-bottom {
-  margin-bottom: 10px;
-}
-.border-radius-5 {
+.calendar_default_event_inner {
+  background: #2e78d6;
+  color: white;
   border-radius: 5px;
-}
+  opacity: 0.9;}
 
-.margin-10px-top {
-  margin-top: 0px;
-}
-.font-size14 {
-  font-size: 14px;
-}
-
-.text-light-gray {
-  color: #d6d5d5;
-}
-.font-size13 {
-  font-size: 13px;
-}
-
-.table-bordered td,
-.table-bordered th {
-  border: 1px solid #dee2e6;
-}
-.table td,
-.table th {
-  padding: 0.75rem;
-  vertical-align: top;
-  border-top: 1px solid #dee2e6;
-}
 </style>
