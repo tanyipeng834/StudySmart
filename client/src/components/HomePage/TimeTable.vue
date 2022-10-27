92282885d7683e88cda3493969f3c092b0d790bd
xhihyu <xhihyuxxx@gmail.com>
2022-10-27 21:27:51 +0800

    finally added timetable :)

--- client/src/components/HomePage/TimeTable.vue
+++ client/src/components/HomePage/TimeTable.vue
@@ -2,53 +2,54 @@
   <div class="wrap">
     <div class="content">
       <h1>Drag and Hold to add new schedule!</h1>
-      <DayPilotCalendar id="dp" :config="calendarConfig" ref="calendar"/>
+      <DayPilotCalendar id="dp" :config="calendarConfig" ref="calendar" />
     </div>
   </div>
 </template>
 
 <script>
-import { DayPilot, DayPilotCalendar, DayPilotNavigator } from "@daypilot/daypilot-lite-vue";
+import {
+  DayPilot,
+  DayPilotCalendar,
+  DayPilotNavigator,
+} from "@daypilot/daypilot-lite-vue";
 
 // Database setup
 import { initializeApp } from "firebase/app";
+import { auth, db } from "../../main";
 import {
-        getFirestore,
-        doc,
-        updateDoc,
-        getDoc,
-        setDoc,
-        collection,
-        addDoc,
-        deleteDoc,
-        deleteField,
-        arrayUnion,
-        arrayRemove,
-        onSnapshot,
-        query,
-        where
-    } from "firebase/firestore";
-
-
-
-
+  getFirestore,
+  doc,
+  updateDoc,
+  getDocs,
+  setDoc,
+  collection,
+  addDoc,
+  deleteDoc,
+  deleteField,
+  arrayUnion,
+  arrayRemove,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
 
 export default {
   name: "ResourceCalendar",
+
   data: function () {
     return {
-        
       calendarConfig: {
         viewType: "Resources",
-        durationBarVisible : false,
+        durationBarVisible: false,
         timeRangeSelectedHandling: "Enabled",
-        columns : [
-        { name: "Monday", id: "R1" },
-        { name: "Tuesday", id: "R2" },
-        { name: "Wednesday", id: "R3" },
-        { name: "Thursday", id: "R4" },
-        { name: "Friday", id: "R5" },
-      ],
+        columns: [
+          { name: "Monday", id: "R1" },
+          { name: "Tuesday", id: "R2" },
+          { name: "Wednesday", id: "R3" },
+          { name: "Thursday", id: "R4" },
+          { name: "Friday", id: "R5" },
+        ],
         onTimeRangeSelected: async (args) => {
           await this.createEvent(args.start, args.end, args.resource);
           this.calendar.clearSelection();
@@ -59,35 +60,33 @@ export default {
         eventDeleteHandling: "Disabled",
         onEventMoved: (args) => {
           this.editEvent(args.e);
-          
-          
+
           // console.log("Event moved", args.e);
         },
         onEventResized: (args) => {
           console.log("Event resized", args.e);
         },
-
-  
-
-
       },
-      
-      eventlist:[{
-    start: DayPilot.Date.today().addHours(10),
-    end: DayPilot.Date.today().addHours(11),
-    id: '1',
-    text: "Event 1",
-    resource: "R5",
-    backColor: "#f57542",
-  },  {
-    start:DayPilot.Date.today().addHours(10), 
-    end:DayPilot.Date.today().addHours(10), 
-    id: 1, 
-    text: "Meeting",
-    resource: "R3"
-  } ]
-      
-    }
+
+      eventlist: [
+        {
+          start: DayPilot.Date.today().addHours(10),
+          end: DayPilot.Date.today().addHours(11),
+          id: "1",
+          text: "Event 1",
+          resource: "R5",
+          backColor: "#f57542",
+        },
+        {
+          start: DayPilot.Date.today().addHours(10),
+          end: DayPilot.Date.today().addHours(10),
+          id: 1,
+          text: "Meeting",
+          resource: "R3",
+        },
+      ],
+      eventid: [],
+    };
   },
   props: {},
   components: {
@@ -100,25 +99,42 @@ export default {
     },
   },
   methods: {
-    loadEvents() {
+    async loadEvents() {
       // placeholder for an HTTP call
-      const events = this.eventlist
-      this.calendar.update({events});
+      var email = localStorage.getItem("email");
+
+      const querySnapshot = await getDocs(collection(db, "users", email, "timetable"));
+      querySnapshot.forEach((doc) => {
+        // doc.data() is never undefined for query doc snapshots
+        var event = {};
+        var hour = doc.data().startHour;
+        event.start = DayPilot.Date.today()
+          .addHours(hour)
+          .addMinutes(doc.data().startMinute);
+
+        event.end = DayPilot.Date.today()
+          .addHours(doc.data().endHour)
+          .addMinutes(doc.data().startMinute);
+        event.resource = doc.data().resource;
+        event.id = doc.data().id;
+        event.text = doc.data().subject;
+        event.backColor = doc.data().backColor;
+
+        this.eventlist.push(event);
+      });
+      const events = this.eventlist;
+      this.calendar.update({ events });
     },
 
-
     loadResources() {
-       var dp = document.getElementById('dp')
-  dp.dayBeginsHour = 9;
-  dp.dayEndsHour = 6;
-      
-      
-
+      var dp = document.getElementById("dp");
+      dp.dayBeginsHour = 9;
+      dp.dayEndsHour = 6;
     },
     async editEvent(e) {
       const form = [
         { name: "Subject", id: "text" },
-        
+
         {
           name: "Day",
           id: "resource",
@@ -133,13 +149,24 @@ export default {
       } else {
         this.calendar.events.update(modal.result);
       }
-
-    
     },
     async createEvent(start, end, resource) {
       const form = [
         { name: "Subject", id: "text" },
-        {name:'Color',id:'Color',type:'select',options:[{name:'Red',id:'#FFADAD'},{name:'Orange',id:'#FFD6A5'},{name:'Yellow',id:'#FDFFB6'},{name:'Green',id:'#CAFFBF'},{name:'Blue',ID:'#9BF6FF'},{name:'Purple',id:'#BDB2FF'},{name:'Pink',id:'#FFC6FF'}]},
+        {
+          name: "Color",
+          id: "Color",
+          type: "select",
+          options: [
+            { name: "Red", id: "#FFADAD" },
+            { name: "Orange", id: "#FFD6A5" },
+            { name: "Yellow", id: "#FDFFB6" },
+            { name: "Green", id: "#CAFFBF" },
+            { name: "Blue", ID: "#9BF6FF" },
+            { name: "Purple", id: "#BDB2FF" },
+            { name: "Pink", id: "#FFC6FF" },
+          ],
+        },
         {
           name: "Day",
           id: "resource",
@@ -151,37 +178,39 @@ export default {
         start,
         end,
         resource,
-        id: DayPilot.guid()
-      
-        
+        id: DayPilot.guid(),
       };
       const modal = await DayPilot.Modal.form(form, data);
       if (modal.canceled) {
         return;
       }
- 
-      
+
+      var email = localStorage.getItem("email");
       const e = modal.result;
-      e.backColor=modal.result.Color
-     
-     
+      e.backColor = modal.result.Color;
+      var color = e.Color;
+
+      const eventData = {
+        startHour: e.start.slice(11, 13),
+        endHour: e.end.slice(11, 13),
+        startMinute: e.start.slice(14, 16),
+        endMinute: e.end.slice(14, 16),
+        resource: e.resource,
+        id: e.id,
+        backColor: color,
+        subject: e.text,
+      };
       this.calendar.events.add(e);
-      await setDoc(doc(db, "users", email, 'countDown'), this.eventlist)
-      
-      
 
-      
-      
+      await setDoc(doc(db, "users", email, "timetable", eventData.subject), eventData);
 
+      this.eventid.push(eventData.subject);
     },
-    
   },
   mounted() {
     this.loadResources();
     this.loadEvents();
-    console.log(this)
-    
-
+    console.log(this.eventlist);
   },
 };
 </script>
@@ -199,13 +228,12 @@ export default {
   flex-grow: 1;
 }
 
-
 .calendar_default_event_inner {
   background: #2e78d6;
   color: black;
   text-decoration: solid;
   text-align: center;
   border-radius: 6px;
- 
 }
 </style>
+
