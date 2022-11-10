057e412003aef8d51975d1b1e7137a6a0ec80c99
Yi Peng <tanyipeng835@hotmail.com>
2022-11-10 21:18:52 +0800

    Starting on quiz page

---
+++ client/src/views/Quiz.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class="container-fluid p-0 main">
+      <Topbar :tabs="tabs" menuTitle="Forum" />
+      <div class="row">
+        <div class="col-4">
+          <Sidebar :haveTopbar="true" />
+        </div>
+        <div class="col-4"></div>
+        <div class="col-4"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Quiz",
+};
+</script>
